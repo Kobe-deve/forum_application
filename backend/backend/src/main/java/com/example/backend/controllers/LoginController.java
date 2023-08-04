75006544fbd4adc28057fa8bd0228c22721e4ffb
Kobe <koberunnels@gmail.com>
2023-08-03 23:01:19 -0500

    added basic login functionality

---
+++ backend/backend/src/main/java/com/example/backend/controllers/LoginController.java
@@ -0,0 +1,32 @@
+package com.example.backend.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.example.backend.models.User;
+import com.example.backend.repository.UserRepository;
+
+@RestController
+@RequestMapping("/login")
+public class LoginController {
+    @Autowired
+    private UserRepository UserRepository;
+    
+    // log into an account
+    @PostMapping 
+    public boolean loginWithCredentials(@RequestBody User user)
+    {
+        List<User> usernames = UserRepository.findByUsername(user.username);
+
+        // check if username exists 
+        if(usernames.size() == 1 && user.password.equals(usernames.get(0).password))
+        {
+            
+            return true;
+        }
+        else
+            return false;
+    }
+}
