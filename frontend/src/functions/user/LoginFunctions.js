cb3f7da501cc3ecee8e014d0c71ed19ce8e90b53
Kobe <koberunnels@gmail.com>
2023-08-16 15:52:46 -0500

    login page state added

--- frontend/src/functions/user/LoginFunctions.js
+++ frontend/src/functions/user/LoginFunctions.js
@@ -1,5 +1,14 @@
 // calling login endpoint
-function callLogin(username, password, callback)
+export function callLogin(username, password, callback)
 {
-
+    setTimeout(()=>{
+        if(username === 'test' && password==='test')
+        {
+            return callback(null);
+        }
+        else
+        {
+            return callback(new Error('Invalid username or password'))
+        }
+    },1000);
 }
--- frontend/src/pages/front/user/Login.js
+++ frontend/src/pages/front/user/Login.js
@@ -1,13 +1,32 @@
 import { useState } from 'react';
+import {callLogin} from '../../../functions/user/LoginFunctions';
 
 export default function Login() {
 
   const [username,setUsername] = useState("");
   const [password,setPassword] = useState("");
 
+  const [pendingLogin,setPendingLogin] = useState(false);
+  const [successLogin,setSuccessLogin] = useState(false);
+  const [loginError,setErrorLogin] = useState(false);
+
   const submitLogin = (e) => {
     console.log(username + " " + password);
+    setSuccessLogin(false);
+    setPendingLogin(true);
+    setErrorLogin(false);
+
+    callLogin(username,password,error => {
+      setPendingLogin(false);
+      if (!error) {
+        setSuccessLogin(true);
+      } else {
+        setErrorLogin(error);
+      }
+    });
+    
     e.preventDefault();
+
   }
 
   return(
@@ -16,6 +35,10 @@ export default function Login() {
         <input type="text" onChange={e=> setUsername(e.target.value)} placeholder='Username'/>
         <input type="password" onChange={e=> setPassword(e.target.value)} placeholder='Password'/>
         <input type="submit" value="Login"/>
+
+        {pendingLogin && <div>Logging in...</div>}
+        {successLogin && <div>Logged in</div>}
+        {loginError && <div>Could not log in</div>}
       </form>
     );
 }
