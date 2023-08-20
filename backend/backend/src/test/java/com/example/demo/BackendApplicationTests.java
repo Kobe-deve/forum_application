0c78e416dde1b655412af0ecc547031eabd42b74
Kobe <koberunnels@gmail.com>
2023-08-20 17:25:02 -0500

    temp solution

--- backend/backend/src/test/java/com/example/demo/BackendApplicationTests.java
+++ backend/backend/src/test/java/com/example/demo/BackendApplicationTests.java
@@ -1,13 +1,7 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.context.SpringBootTest;
 
-import com.example.backend.BackendApplication;
-
-@SpringBootTest(classes = BackendApplication.class)
-@AutoConfigureTestDatabase
 class BackendApplicationTests {
 
 	@Test
