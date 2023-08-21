01007feb9016e4c62b1aac8c277b943751150340
Kobe <koberunnels@gmail.com>
2023-08-21 18:49:54 -0500

    added coverage report under backend\backend\build\jacocoHtml

--- backend/backend/src/test/java/com/example/demo/BackendApplicationTests.java
+++ backend/backend/src/test/java/com/example/demo/BackendApplicationTests.java
@@ -1,11 +1,32 @@
 package com.example.demo;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import com.example.backend.BackendApplication;
+import com.example.backend.controllers.UserController;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(
+    classes = BackendApplication.class,
+    webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
+)
 class BackendApplicationTests {
+
+	@Autowired
+	private UserController controller;
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void controllerContextLoads() throws Exception {
+		assertThat(controller).isNotNull();
 	}
 
 }
