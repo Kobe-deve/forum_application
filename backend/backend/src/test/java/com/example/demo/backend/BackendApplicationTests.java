fa100a1a243e4e0c82782ea96963f95a4b7d74ba
Kobe <koberunnels@gmail.com>
2023-08-24 23:18:47 -0500

    successful api endpoint call test

--- backend/backend/src/test/java/com/example/demo/backend/BackendApplicationTests.java
+++ backend/backend/src/test/java/com/example/demo/backend/BackendApplicationTests.java
@@ -1,7 +1,6 @@
 package com.example.demo.backend;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 
 import com.example.backend.BackendApplication;
--- backend/backend/src/test/java/com/example/demo/backend/UserApiTest.java
+++ backend/backend/src/test/java/com/example/demo/backend/UserApiTest.java
@@ -4,26 +4,28 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
 import com.example.backend.BackendApplication;
-import com.example.backend.models.User;
+import com.example.backend.controllers.FriendListController;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.nio.charset.Charset;
 
 @WebMvcTest(BackendApplication.class)
+@Import(FriendListController.class)
 @AutoConfigureMockMvc(addFilters=false)
 @EnableWebMvc
 @ContextConfiguration(classes= BackendApplication.class)
@@ -42,8 +44,12 @@ public class UserApiTest {
     public static final MediaType APPLICATION_JSON_UTF8 = new MediaType(MediaType.APPLICATION_JSON.getType(), MediaType.APPLICATION_JSON.getSubtype(), Charset.forName("utf8"));
 
     @Test
-	void contextLoads() {
-        assertNotNull(mockMvc);
-	}
-
+    void contextLoads() {
+          assertNotNull(mockMvc);
+    }
+    
+    @Test
+    void getRequestTest() throws Exception {
+      this.mockMvc.perform(MockMvcRequestBuilders.get("/friends/list")).andDo(print()).andExpect(status().isOk());
+    }
 }
