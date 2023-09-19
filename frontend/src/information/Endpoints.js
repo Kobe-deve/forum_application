299462eb058108c6831821c2079c6ba419d05b70
Kobe <koberunnels@gmail.com>
2023-09-19 16:53:48 -0500

    set frontend to route through gateway

--- frontend/src/information/Endpoints.js
+++ frontend/src/information/Endpoints.js
@@ -1,24 +1,10 @@
 // the host endpoint to call
-var host = "http://localhost:8081"
-var socketHost = "ws://localhost:8081"
+var host = "http://localhost:4000"
+var socketHost = "ws://localhost:8081/sockettest"
 
 // holds all urls to access
 export var urls = {
-    "LOGIN": host+"/users/login",
-    "SIGNUP": host+"/users/signup",
-    "AUTHENTICATE": host+"users/authenticate",
-    "USER_INFO": host+"/users/",
-    "CREATE_ROOM":host+"/rooms/createRoom",
-    "ROOM_INFO":host+"/rooms/",
-    "SEND_MESSAGE":host+"/message/send",
-    "GET_MESSAGES":host+"/message/",
-    "RECENT_MESSAGE":host+"/message/recent/",
-    "REPORT_MESSAGE":host+"/message/report",
-    "FRIEND_LIST":host+"/friends/list",
-    "VIEW_FRIEND_REQUESTS":host+"/friends/requests",
-    "SEND_FRIEND_REQUESTS":host+"/friends/request/",
-    "REMOVE_FRIEND":host+"/friends/remove/",
-    "ACCEPT_FRIEND_REQUEST":host+"/friends/accept/",
-
-    "MESSAGE_ROOM":socketHost+"/sockettest"
+    "LOGIN": host+"/login",
+    "SIGNUP": host+"/signup",
+    "MESSAGE_ROOM":socketHost
 };
--- gateway/routes.js
+++ gateway/routes.js
@@ -1,32 +1,75 @@
 const ROUTES = [
     {
-        url: '/free',
+        url: '/login',
         auth: false,
         creditCheck: false,
         rateLimit: {
-            windowMs: 1 * 60 * 1000,
+            windowMs: 15 * 60 * 1000,
             max: 5
         },
         proxy: {
-            target: "https://www.google.com",
+            target: "http://localhost:8081/users/login",
             changeOrigin: true,
             pathRewrite: {
-                [`^/free`]: '',
+                [`^/login`]: '',
+            },
+        }
+    },
+	{
+        url: '/signup',
+        auth: false,
+        creditCheck: false,
+        rateLimit: {
+            windowMs: 15 * 60 * 1000,
+            max: 5
+        },
+        proxy: {
+            target: "http://localhost:8081/users/signup",
+            changeOrigin: true,
+            pathRewrite: {
+                [`^/signup`]: '',
             },
         }
     },
     {
-        url: '/premium',
+        url: '/socket',
         auth: true,
         creditCheck: true,
         proxy: {
-            target: "https://www.google.com",
+            target: "ws://localhost:8081/sockettest",
             changeOrigin: true,
             pathRewrite: {
-                [`^/premium`]: '',
+                [`^/socket`]: '',
             },
         }
     }
 ]
+
+/*
+// the host endpoint to call
+var host = "http://localhost:8081"
+var socketHost = "ws://localhost:8081"
+
+// holds all urls to access
+export var urls = {
+    "LOGIN": host+"/users/login",
+    "SIGNUP": host+"/users/signup",
+    "AUTHENTICATE": host+"users/authenticate",
+    "USER_INFO": host+"/users/",
+    "CREATE_ROOM":host+"/rooms/createRoom",
+    "ROOM_INFO":host+"/rooms/",
+    "SEND_MESSAGE":host+"/message/send",
+    "GET_MESSAGES":host+"/message/",
+    "RECENT_MESSAGE":host+"/message/recent/",
+    "REPORT_MESSAGE":host+"/message/report",
+    "FRIEND_LIST":host+"/friends/list",
+    "VIEW_FRIEND_REQUESTS":host+"/friends/requests",
+    "SEND_FRIEND_REQUESTS":host+"/friends/request/",
+    "REMOVE_FRIEND":host+"/friends/remove/",
+    "ACCEPT_FRIEND_REQUEST":host+"/friends/accept/",
+
+    "MESSAGE_ROOM":socketHost+"/sockettest"
+};
+*/
 
 exports.ROUTES = ROUTES;
