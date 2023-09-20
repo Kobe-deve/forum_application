ee70f9cbf93b9feaa9bd9e99f503b3431e5afe23
Kobe <koberunnels@gmail.com>
2023-09-19 23:19:04 -0500

    removed previous socket endpoint

--- messaging/index.js
+++ messaging/index.js
@@ -5,25 +5,24 @@ const wss = new WebSocketServer({ port: 8080 });
 
 wss.on('connection', async function connection(ws) {
   
-  const client = new Client({
-	  user: process.env.DATABASE_USERNAME,
-	  host: process.env.DATABASE_HOST,
-	  database: process.env.DATABASE_NAME,
-	  password: process.env.DATABASE_PASSWORD,
-	  port: process.env.DATABASE_PORT,
+	const client = new Client({
+		user: process.env.DATABASE_USERNAME,
+		host: process.env.DATABASE_HOST,
+		database: process.env.DATABASE_NAME,
+		password: process.env.DATABASE_PASSWORD,
+		port: process.env.DATABASE_PORT,
 	})  	  
 	
+	// initial connection 
 	client.connect(function(err) {
-	  if (err) throw err;
-	  console.log("Connected!");
-	  
+		if (err) throw err;	  
 	});
     
-  ws.on('message', async function message(data) {
-	 const res = await client.query('Select * from message_log where room_id=1;')
-	 let response = {"messages": JSON.stringify(res.rows)}
-     console.log(response)
-	
-	 ws.send(JSON.stringify(response));
-  });
+	// response based on messages sent 
+    ws.on('message', async function message(data) {
+		const res = await client.query('Select * from message_log where room_id=1;')
+		let response = {"messages": JSON.stringify(res.rows)}
+
+		ws.send(JSON.stringify(response));
+    });
 });
